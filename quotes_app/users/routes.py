b1e8c23902eac9a4a4428455bba46eefe108cd35
sebISU <Derendarz99@gmail.com>
2021-02-04 10:35:09 +0100

    save before refactorization

--- quotes_app/users/routes.py
+++ quotes_app/users/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, flash, request, url_for
 from flask_login import login_user, logout_user, current_user, login_required
 from quotes_app import db, bcrypt
-from quotes_app.models import User, Post
+from quotes_app.models import User, Post, Like, Comment
 from quotes_app.users.forms import (RegistrationForm, LoginForm, ResetRequestForm,
                                 ResetPasswordForm)
 from quotes_app.users.utils import send_reset_token
@@ -44,6 +44,22 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('main.home'))
+
+@users.route("/user/<string:username>")
+@login_required
+def user_profile(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(posted_by=user)\
+        .order_by(Post.date_posted.desc())
+    num_posts = posts.all()
+    num_posts = len(num_posts)
+    num_likes = len(Like.query.filter_by(like_author=user).all())
+    num_comments = len(Comment.query.filter_by(comment_author=user).all())
+    posts = posts.paginate(page=page, per_page=5)
+    return render_template('user_profile.html', title=username, posts=posts, user=user,
+        num_posts=num_posts, num_likes=num_likes, num_comments=num_comments)
+
 
 @users.route("/reset_password", methods=['GET', 'POST'])
 def reset_request():
